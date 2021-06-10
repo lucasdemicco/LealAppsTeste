b071badefc8227f98289872a5b1a34998f3efc34
lucasdemicco <lucasmicco@hotmail.com>
2021-06-10 10:44:20 -0300

    Add página de cadastro

---
+++ app/src/main/java/View/CadastrarActivity.java
@@ -0,0 +1,20 @@
+package View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lucas.lealappsteste.R;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CadastrarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar);
+    }
+
+    public void realizarCadastro(View view) {
+    }
+}
--- app/src/main/java/View/LoginActivity.java
+++ app/src/main/java/View/LoginActivity.java
@@ -2,15 +2,37 @@ package View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.lucas.lealappsteste.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button btnEntrar, btnCadastrar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+    }
+
+    public void login(View view) {
+    }
+
+    public void cadastrar(View view) {
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CadastrarActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
