d9869167afdd08063ac677029ae0acb6ea69a626
lucasdemicco <lucasmicco@hotmail.com>
2021-06-11 11:46:12 -0300

    "Recuperação de imagem URI Galeria"

--- app/src/main/java/View/TreinosActivity.java
+++ app/src/main/java/View/TreinosActivity.java
@@ -1,11 +1,17 @@
 package View;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,9 +19,12 @@ import com.lucas.lealappsteste.R;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import Controller.Permissoes;
 
-public class TreinosActivity extends AppCompatActivity {
+public class TreinosActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView imgTreino;
 
@@ -24,15 +33,49 @@ public class TreinosActivity extends AppCompatActivity {
             Manifest.permission.CAMERA
     };
 
+    private List<String> listaFotosRecuperadas = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_treinos);
 
         imgTreino = findViewById(R.id.imgTreino);
+        imgTreino.setOnClickListener(this);
 
         //Validar permissões
         Permissoes.validarPermissoes(permissoes, this, 1);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imgTreino:
+                escolherImagem(1);
+                break;
+        }
+    }
+
+    public void escolherImagem(int requestCode){
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, requestCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Recuperar Imagem
+        if(resultCode == Activity.RESULT_OK){
+            Uri imagemSelecionada = data.getData();
+            String caminhoImagem = imagemSelecionada.toString();
+
+            //Configurar imagem no IMageView
+            if(requestCode == 1){
+                imgTreino.setImageURI(imagemSelecionada);
+                listaFotosRecuperadas.add(caminhoImagem);
+            }
+        }
     }
 
     @Override
@@ -61,5 +104,8 @@ public class TreinosActivity extends AppCompatActivity {
         AlertDialog dialog = builder.create();
         dialog.show();
 
+    }
+
+    public void salvarTreino(View view) {
     }
 }
