7965ee13d4f06698ab3ff646d882ff2064d7dc44
lucasdemicco <lucasmicco@hotmail.com>
2021-06-11 11:28:44 -0300

    "Add Permissioes Codigo Java"

--- app/src/main/java/View/TreinosActivity.java
+++ app/src/main/java/View/TreinosActivity.java
@@ -1,15 +1,28 @@
 package View;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.lucas.lealappsteste.R;
 
+import org.jetbrains.annotations.NotNull;
+
+import Controller.Permissoes;
+
 public class TreinosActivity extends AppCompatActivity {
 
     private ImageView imgTreino;
+
+    private String [] permissoes = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +30,36 @@ public class TreinosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_treinos);
 
         imgTreino = findViewById(R.id.imgTreino);
+
+        //Validar permissões
+        Permissoes.validarPermissoes(permissoes, this, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for(int permissaoResultado : grantResults){
+            if(permissaoResultado == getPackageManager().PERMISSION_DENIED){
+                alertaPermissao();
+            }
+        }
+    }
+
+    public void alertaPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões negadas");
+        builder.setMessage("Para utilizar é necessário aceitar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 }
